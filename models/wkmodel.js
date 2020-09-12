0d5edfe01ec81add02207d586fcadf25b39ac5b7
Samir Bello <agnide4@gmail.com>
2020-09-12 14:01:06 -0500

    Models created

---
+++ models/wkmodel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+    }],
+
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+
+  });
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function() {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -12,12 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercise", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 const db = require("./models");
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
